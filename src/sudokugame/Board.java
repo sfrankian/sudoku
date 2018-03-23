553e0c24bd1b30538908f4882d5844ee9fd17024
Stephanie Frankian <sfrankia@wellesley.edu>
2018-03-23 15:01:05 -0400

    Finished basic board functinality

--- src/sudokugame/Board.java
+++ src/sudokugame/Board.java
@@ -1,5 +1,7 @@
 package sudokugame;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Board {
@@ -9,31 +11,63 @@ public class Board {
 	public Board(String fileName) {
 		this.fileName = fileName;
 		// Loading gameBoard from text file
-		int[][] gameBoard = new int[9][9];
+		
+		gameBoard = new int[9][9];
+		loadSudokuFromFile(fileName);
 	}
 	
+	/**
+	 * Helper function to load a sudoku base game from file.
+	 * Expects each row of the sudoku board to be space-delimited
+	 * @param fileName
+	 */
 	public void loadSudokuFromFile(String fileName) {
-		Scanner scan = new Scanner(fileName);
+		File f = new File(fileName);
 		
-		int col = 0;
-		while(scan.hasNext()) {
-			String currLine = scan.nextLine();
-			String[] currLineAsArr = currLine.split(" ");
-			
-			for(int i = 0; i < 9; i++) {
-				String currChar = currLineAsArr[i];
-				int currVal = Integer.parseInt(currChar);
+		Scanner scan;
+		try {
+			scan = new Scanner(f);
+			int col = 0;
+			while(scan.hasNext()) {
+				String currLine = scan.nextLine();
+				String[] currLineAsArr = currLine.split(" ");
+				System.out.println(currLine);
+				for(int i = 0; i < 9; i++) {
+					String currChar = currLineAsArr[i];
+					int currVal = Integer.parseInt(currChar);
+					
+					gameBoard[i][col] = currVal;
+				}
 				
-				gameBoard[i][col] = currVal;
+				col++;
 			}
-			
-			col++;
+			scan.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+		
+	}
+	
+	public void addNumberToBoard(int xIndex, int yIndex, int value) {
+		gameBoard[xIndex][yIndex] = value;
 	}
 	
 	public String toString() {
 		String output = "";
+		
+		for(int i = 0; i < 9 ; i++) {
+			for(int j = 0; j < 9; j++) {
+				output += gameBoard[i][j] + " ";
+			}
+			output += "\n";
+		}
 		return output;
+	}
+	
+	public static void main(String[] args) {
+		Board b = new Board("assets/sudokugrid.txt");
+		b.toString();
 	}
 }
